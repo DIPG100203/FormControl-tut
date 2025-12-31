555ba2fc3c6653d18aa0fa64993dc5e84288294b
Daniel Ivan Guerrero Ponce <danielivanguerreroponce@gamil.com>
2025-12-31 11:33:57 -0600

    uso del formGroup

--- src/app/basic/basic.component.ts
+++ src/app/basic/basic.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators, FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -11,23 +11,22 @@ import { CommonModule } from '@angular/common';
 })
 export class BasicComponent implements OnInit {
 
-  nameField = new FormControl('', [Validators.required, Validators.maxLength(10)]);
-  emailField = new FormControl('')
-  phoneField = new FormControl('')
-  colorField = new FormControl('')
-  dateField = new FormControl('')
-  numberField = new FormControl('')
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl(''),
+    color: new FormControl(''),
+    date: new FormControl(''),
+    number: new FormControl(''),
 
-  // selects 
+    category: new FormControl(''),
+    tag: new FormControl(''),
 
-  categroyField = new FormControl('')
-  tagField = new FormControl('')
+    agreed: new FormControl(false),
+    gender: new FormControl(''),
+    zone: new FormControl('')
+  })
 
-  //checkboxes y radios
-
-  agreedField = new FormControl(false)
-  genderField = new FormControl('')
-  zoneField = new FormControl('')
 
   // ejercicio 1
 
@@ -48,6 +47,83 @@ export class BasicComponent implements OnInit {
   // método para obtener el valor del campo al hacer clic en el botónn
   getNameValue() {
     console.log(this.nameField.value);
+  }
+
+  //ejercicio 2: desarrollar un metodo que no permita enviar datos si estan vacios o invalidos
+
+  save(event: Event) {
+    this.form.markAllAsTouched();
+    if (this.form.invalid) {
+      return
+    }
+    console.log(this.form.value)
+  }
+
+  /* save(event: Event) {
+    console.log(this.form.value)
+  } */
+ 
+
+  get nameField() {
+    return this.form.get('name')!;
+  }
+
+  get emailField() {
+    return this.form.get('email')!;
+  }
+
+  get phoneField() {
+    return this.form.get('phone')!;
+  }
+
+  get colorField() {
+    return this.form.get('color')!;
+  }
+
+  get dateField() {
+    return this.form.get('date')!;
+  }
+
+  get numberField() {
+    return this.form.get('number')!;
+  }
+
+  get categoryField() {
+    return this.form.get('category')!;
+  }
+
+  get tagField() {
+    return this.form.get('tag')!;
+  }
+
+  get agreedField() {
+    return this.form.get('agreed')!;
+  }
+
+  get genderField() {
+    return this.form.get('gender')!;
+  }
+
+  get zoneField() {
+    return this.form.get('zone')!;
+  }
+
+  get isNameFieldValid() {
+    return this.nameField.touched && this.nameField.valid
+  }
+
+  get isNameFieldInvalid() {
+    return this.nameField.touched && this.nameField.invalid
+  }
+
+  // ejercicio 2 aqui seria para el correo
+
+  get isEmailFieldValid() {
+    return this.emailField.touched && this.emailField.valid
+  }
+
+  get isEmailFieldInvalid() {
+    return this.emailField.touched && this.emailField.invalid
   }
 
 }
