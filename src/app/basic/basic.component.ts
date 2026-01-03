3537c7187362b3875a42ff5483f9210ce652cb7d
Daniel Ivan Guerrero Ponce <danielivanguerreroponce@gamil.com>
2026-01-02 20:31:37 -0600

    Implementacion de Material

--- src/app/basic/basic.component.ts
+++ src/app/basic/basic.component.ts
@@ -1,38 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, ReactiveFormsModule, Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  FormControl,
+  ReactiveFormsModule,
+  Validators,
+  FormGroup,
+  FormBuilder,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
 
 @Component({
   selector: 'app-basic',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatRadioModule
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [MatDatepickerModule],
   templateUrl: './basic.component.html',
-  styleUrl: './basic.component.css'
+  styleUrl: './basic.component.css',
 })
 export class BasicComponent implements OnInit {
-
-  form!: FormGroup
-
+  form!: FormGroup;
 
   // ejercicio 1
 
-  seguroField = new FormControl(false)
-  preferenciaField = new FormControl('')
+  seguroField = new FormControl(false);
+  preferenciaField = new FormControl('');
 
-  constructor(
-    private fb: FormBuilder
-  ) {
-    this.buildForm()
+  constructor(private fb: FormBuilder) {
+    this.buildForm();
   }
 
   ngOnInit(): void {
     // escuchar los cambios en el valor del campo en tiempo real
-    this.nameField.valueChanges
-    .subscribe(value => {
+    this.nameField.valueChanges.subscribe((value) => {
       console.log(value);
-    })
+    });
   }
-
 
   // método para obtener el valor del campo al hacer clic en el botónn
   getNameValue() {
@@ -44,9 +65,9 @@ export class BasicComponent implements OnInit {
   save(event: Event) {
     this.form.markAllAsTouched();
     if (this.form.invalid) {
-      return
+      return;
     }
-    console.log(this.form.value)
+    console.log(this.form.value);
   }
 
   /* save(event: Event) {
@@ -54,26 +75,49 @@ export class BasicComponent implements OnInit {
   } */
 
   private buildForm() {
-
     this.form = this.fb.group({
-      name: ['', [Validators.required, Validators.maxLength(19), Validators.pattern('[a-zA-Z ]*')]],
+      fullName: this.fb.group({
+        name: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(1),
+            Validators.maxLength(10),
+            Validators.pattern('[a-zA-Z ]*'),
+          ],
+        ],
+        lastName: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(1),
+            Validators.pattern('[a-zA-Z ]*'),
+          ],
+        ],
+      }),
       email: ['', [Validators.required, Validators.email]],
       phone: ['', Validators.required],
       color: ['#000000'],
       date: [''],
-      number: [0, [Validators.required, Validators.min(18), Validators.max(100)]],
+      number: [
+        0,
+        [Validators.required, Validators.min(18), Validators.max(100)],
+      ],
       category: [''],
       tag: [''],
 
       agreed: [false, Validators.requiredTrue],
       gender: [''],
-      zone: ['']
-    })
+      zone: [''],
+    });
   }
- 
 
   get nameField() {
-    return this.form.get('name')!;
+    return this.form.get('fullName.name')!;
+  }
+
+  get lastNameField() {
+    return this.form.get('fullName.lastName')!;
   }
 
   get emailField() {
@@ -117,25 +161,36 @@ export class BasicComponent implements OnInit {
   }
 
   get isNameFieldValid() {
-    return this.nameField.touched && this.nameField.valid
+    return this.nameField.touched && this.nameField.valid;
   }
 
   get isNameFieldInvalid() {
-    return this.nameField.touched && this.nameField.invalid
+    return this.nameField.touched && this.nameField.invalid;
   }
 
   // ejercicio 2 aqui seria para el correo
 
   get isEmailFieldValid() {
-    return this.emailField.touched && this.emailField.valid
+    return this.emailField.touched && this.emailField.valid;
   }
 
   get isEmailFieldInvalid() {
-    return this.emailField.touched && this.emailField.invalid
+    return this.emailField.touched && this.emailField.invalid;
   }
 
   get isPhoneFieldValid() {
-    return this
+    return this;
   }
 
+  get isPhoneFieldInvalid() {
+    return this.phoneField.touched && this.phoneField.invalid;
+  }
+
+  get isNumberFieldValid() {
+    return this.numberField.touched && this.numberField.valid;
+  }
+
+  get isNumberFieldInvalid() {
+    return this.numberField.touched && this.numberField.invalid;
+  }
 }
